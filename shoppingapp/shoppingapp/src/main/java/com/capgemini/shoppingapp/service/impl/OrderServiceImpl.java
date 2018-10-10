cb329b9a4dc9b2ad6a76f373092d7e7109fc0e0d
Srivastava <shubham.a.srivastava@capgemini.com>
2018-10-10 07:34:51 -0400

    First commit

--- shoppingapp/shoppingapp/src/main/java/com/capgemini/shoppingapp/service/impl/OrderServiceImpl.java
+++ shoppingapp/shoppingapp/src/main/java/com/capgemini/shoppingapp/service/impl/OrderServiceImpl.java
@@ -3,11 +3,9 @@ package com.capgemini.shoppingapp.service.impl;
 import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Service;
 
 import com.capgemini.shoppingapp.entity.LineItem;
